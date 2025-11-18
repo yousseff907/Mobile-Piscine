71cde62e953bd030d1d6a37c15890b1f98c28484
Youssef <youssef.itani.11.11@gmail.com>
2025-11-18 19:41:07 +0200

    v0.0001

---
+++ mobileModule00/ex02/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main()
+{
+	runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+	@override
+	Widget build(BuildContext context)
+	{
+		return (MaterialApp(debugShowCheckedModeBanner: false, title: "Exerice 01", home: MyHomePage()));
+	}
+}
+
+class MyHomePage extends StatefulWidget
+{
+	@override
+    MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage>
+{
+	String	message = "A simple text";
+	bool	HelloWorld = false;
+	void	toggleText()
+	{
+		setState(()
+		{
+			if (HelloWorld)
+			{
+				message = "A simple text";
+			}
+			else
+			{
+				message = "Hello world";
+			}
+			HelloWorld = !HelloWorld;
+		});
+	}
+
+	@override
+	Widget build(BuildContext context)
+	{
+		return (Scaffold(
+				appBar: AppBar(title: Text("Calculator")),
+				body: Center(
+					child: Column(
+						mainAxisAlignment: MainAxisAlignment.center,
+						children: [Text(message),
+									ElevatedButton(onPressed: ()
+									{
+										toggleText();
+										print("Button pressed");
+									},
+									child: Text("Click me"))],
+									),
+							),
+						)
+				);
+	}
+
+}
